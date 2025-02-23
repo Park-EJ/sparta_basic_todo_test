e4616b246276078b6432d14539864ac9e57eedcd
Park-EJ <sophia5091191@gmail.com>
2025-02-23 10:49:09 +0900

    first commit

---
+++ src/main/java/com/example/basictodotest/BasicTodoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.basictodotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTodoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicTodoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basictodotest/BasicTodoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basictodotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTodoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
